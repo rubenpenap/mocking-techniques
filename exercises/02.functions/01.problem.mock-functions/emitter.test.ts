5e36a4700e7cc5793fc85a9c387875a2d9a100de
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-14 09:41:20 -0400

    test: implement tests for Emitter event handling

--- exercises/02.functions/01.problem.mock-functions/emitter.test.ts
+++ exercises/02.functions/01.problem.mock-functions/emitter.test.ts
@@ -1,26 +1,11 @@
-import { Emitter } from './emitter.js'
+import { Emitter } from './emitter.js';
 
 test('invokes the listener whenever a matching event is emitted', () => {
-  // We have an emitter that can emit and handle the "hello" event,
-  // accepting a "firstName" string as the event data.
-  const emitter = new Emitter<{ hello: [firstName: string] }>()
+	const emitter = new Emitter<{ hello: [firstName: string] }>();
+	const listener = vi.fn();
+	emitter.on('hello', listener);
+	emitter.emit('hello', 'John');
 
-  // 🐨 Create a variable called "listener" and assign it
-  // the result of calling the `vi.fn()` utility. This will
-  // create a mock function!
-
-  // 🐨 Add a listener for the "hello" event using the mock "listener"
-  // function as the argument.
-  // 💰 emitter.on(event, listener)
-
-  // 🐨 Emit the "hello" event with the name "John" as an argument.
-  // 💰 emitter.emit(event, firstName)
-
-  // 🐨 Write an assertion for the mock "listener" function
-  // to have been called once.
-  // 💰 expect(fn).toHaveBeenCalledOnce()
-
-  // 🐨 Write another assertion for the mock "listener" function
-  // to have been called with the string "John" as an argument.
-  // 💰 expect(fn).toHaveBeenCalledWith(arg1, arg2, ...)
-})
+	expect(listener).toHaveBeenCalledOnce();
+	expect(listener).toHaveBeenCalledWith('John');
+});
