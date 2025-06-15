5e6e0b966bdc2377c05d1a2cb49415d6f7810451
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-15 12:13:59 -0400

    test: enhance user creation test with logger assertions

--- exercises/02.functions/02.problem.spies/user-service.test.ts
+++ exercises/02.functions/02.problem.spies/user-service.test.ts
@@ -1,23 +1,18 @@
-import { Logger } from './logger.js'
-import { UserService } from './user-service.js'
+import { Logger } from './logger.js';
+import { UserService } from './user-service.js';
 
 test('logs out the "createUser" event when creating a new user', async () => {
-  const logger = new Logger()
+	const logger = new Logger();
 
-  // 🐨 Spy on the `logger.log` method using the `vi.spyOn()` API.
-  // 💰 vi.spyOn(target, method)
+	vi.spyOn(logger, 'log');
 
-  const service = new UserService(logger)
+	const service = new UserService(logger);
 
-  await service.createUser({
-    id: 'abc-123',
-    name: 'John',
-  })
+	await service.createUser({
+		id: 'abc-123',
+		name: 'John',
+	});
 
-  // 🐨 Write an assertion that `logger.log` has been called with the
-  // "createUser" event and the { id: 'abc-123' } object as metadata.
-  // 💰 expect(spy).toHaveBeenCalledWith(arg1, arg2)
-
-  // 🐨 Write another assertion that `logger.log` has been called once.
-  // 💰 expect(spy).toHaveBeenCalledOnce()
-})
+	expect(logger.log).toHaveBeenCalledWith('createUser', { id: 'abc-123' });
+	expect(logger.log).toHaveBeenCalledOnce();
+});
