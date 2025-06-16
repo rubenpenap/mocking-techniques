cbfbf83e27fd335182170b58a616f9fc48d87955
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-16 19:48:54 -0400

    test: update getAuthToken test to assert resolved token value

--- exercises/05.network/02.problem.mock-responses/get-auth-token.test.ts
+++ exercises/05.network/02.problem.mock-responses/get-auth-token.test.ts
@@ -1,12 +1,10 @@
-import { getAuthToken } from './get-auth-token.js'
+import { getAuthToken } from './get-auth-token.js';
 
 test('returns the authentication token on successful authentication', async () => {
-  // 🐨 Wrap ths `getAuthToken` promise in the `expect` function
-  // and assert that it resolves with an object containing
-  // a `token` property.
-  // 💰 await expect(promise).resolves.toEqual({})
-  await getAuthToken({
-    email: 'kody@epicweb.dev',
-    password: 'supersecret123',
-  })
-})
+	await expect(
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).resolves.toEqual({ token: 'abc-123' });
+});
