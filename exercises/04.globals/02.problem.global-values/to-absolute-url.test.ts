bd76d04558d11d93c18c59f7aed89307edb346eb
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-16 15:18:04 -0400

    test: add beforeAll and afterAll hooks to toAbsoluteUrl tests with global stubbing

--- exercises/04.globals/02.problem.global-values/to-absolute-url.test.ts
+++ exercises/04.globals/02.problem.global-values/to-absolute-url.test.ts
@@ -1,29 +1,18 @@
-import { toAbsoluteUrl } from './to-absolute-url.js'
+import { toAbsoluteUrl } from './to-absolute-url.js';
 
-// 🐨 Add the `beforeAll()` hook and call the `vi.stubGlobal()` utility there.
-// As the first argument, provide it with the string "location", indicating
-// what global object to mock. As the second argument, provide a mock URL
-// "http://localhost/base/" representing the location you want.
-// 💰 beforeAll(callback)
-// 💰 vi.stubGlobal(property, value)
-// 💰 new URL('http://localhost/base/')
+beforeAll(() => {
+	vi.stubGlobal('location', new URL('http://localhost/base/'));
+});
 
-// 🐨 Add the `afterAll()` hook and call the `vi.unstubAllGlobals()` function
-// to restore the patched global values and clean up after yourself.
+afterAll(() => {
+	vi.unstubAllGlobals();
+});
 
 test('returns an absolute url as-is', () => {
-  // 🐨 Call the `toAbsoluteUrl` function with an absolute URL as the argument
-  // and assert that URL is returned as-is.
-  // 💰 expect(toAbsoluteUrl('https://example.com/')).toBe(expected)
-})
+	expect(toAbsoluteUrl('https://example.com/')).toBe('https://example.com/');
+});
 
 test('resolves a relative url against the current location', () => {
-  // 🐨 Call the `toAbsoluteUrl` function with an absolute path (!) as the argument
-  // and assert that URL is resolved against the current location.
-  // 💰 expect(toAbsoluteUrl('/resource')).toBe(expected)
-  //
-  // 🐨 Add another case where the `toAbsoluteUrl` function is called with
-  // a relative path (!). Complete the assertion similarly, keeping in mind
-  // that the relative path will be resolved to a URL including "/base/".
-  // 💰 expect(toAbsoluteUrl('./resource')).toBe(expected)
-})
+	expect(toAbsoluteUrl('/resource')).toBe('http://localhost/resource');
+	expect(toAbsoluteUrl('./resource')).toBe('http://localhost/base/resource');
+});
