38f07a85d46e28181ca0adf6ef8586976fa0e918
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-15 22:49:39 -0400

    test: implement connection event listener test with timer mocking

--- exercises/03.date-and-time/03.problem.ticks-and-tasks/connection.test.ts
+++ exercises/03.date-and-time/03.problem.ticks-and-tasks/connection.test.ts
@@ -1,25 +1,19 @@
-import { Connection } from './connection.js'
+import { Connection } from './connection.js';
 
-// 🐨 Add the `beforeAll()` and `afterAll()` hooks that
-// mock the date using `vi.useFakeTimers()` and
-// `vi.useRealTimers()` respectively.
-// 💰 beforeAll(callback)
-// 💰 afterAll(callback)
+beforeAll(() => {
+	vi.useFakeTimers({ toFake: ['queueMicrotask'] });
+});
 
-// By default, Vitest does not mock "queueMicrotask".
-// 🐨 Configure the `vi.useFakeTimers` to mock "queueMicrotask".
-// 💰 vi.useFakeTimers({ toFake: ['queueMicrotask'] })
+afterAll(() => {
+	vi.useRealTimers();
+});
 
 test('dispatches the "connection" event', () => {
-  // 🐨 Write the test case for the "Connection" instance
-  // dispatching the "connection" event. You'd have to observe
-  // the calls to the connection listener function, so feel free
-  // to utilize everything you've learned up to this point!
-  //
-  // 🐨 Use `vi.runAllTicks()` to exhaust the event loop,
-  // running any scheduled tasks, like the "dispatchEvent()"
-  // from the "Connection" constructor.
-  //
-  // 🐨 Write an assertion that the "connection" event listener
-  // has been called once.
-})
+	const connection = new Connection();
+	const connectionListener = vi.fn();
+	connection.addEventListener('connection', connectionListener);
+
+	vi.runAllTicks();
+
+	expect(connectionListener).toHaveBeenCalledOnce();
+});
