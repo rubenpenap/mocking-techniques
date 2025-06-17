7fa8a3b52a49395c1c30035a93ca1f57723b5788
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-16 20:20:28 -0400

    test: refactor getAuthToken tests to use MSW for error handling and response simulation

--- exercises/05.network/03.problem.error-responses/get-auth-token.test.ts
+++ exercises/05.network/03.problem.error-responses/get-auth-token.test.ts
@@ -1,43 +1,42 @@
-// 🐨 Import the `server` variable from "./mocks/node.js".
-// 🦉 Keep in mind that ESM imports must have ".js" extension
-// even if you are referencing TypeScript files.
-// 💰 import { something } from './somewhere.js'
-import { getAuthToken } from './get-auth-token.js'
+import { http } from 'msw';
+import { server } from './mocks/node.js';
+import { getAuthToken } from './get-auth-token.js';
 
 test('returns the authentication token on successful authentication', async () => {
-  await expect(
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).resolves.toEqual({ token: 'abc-123' })
-})
+	await expect(
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).resolves.toEqual({ token: 'abc-123' });
+});
 
 test('throws an error if the user credentials are invalid', async () => {
-  // 🐨 Call `server.use()` and provide it with a runtime handler.
-  // In that handler, intercept the same "POST https://api.example.com/auth"
-  // request but respond with a 401 response instead.
-  // 💰 server.use(...)
-  // 💰 http.post(path, resolver)
-  // 💰 new Response(null, { status: 401 })
+	server.use(
+		http.post('https://api.example.com/auth', () => {
+			return new Response(null, { status: 401 });
+		}),
+	);
 
-  await expect(() =>
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).rejects.toThrow('Authentication failed: invalid credentials')
-})
+	await expect(() =>
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).rejects.toThrow('Authentication failed: invalid credentials');
+});
 
 test('throws an error if the server responds with an error', async () => {
-  // 🐨 Similarly, call `server.use()` and provide it with another runtime handler,
-  // this time responding with a 500 response.
-  // 💰 new Response(null, { status: 500 })
+	server.use(
+		http.post('https://api.example.com/auth', () => {
+			return new Response(null, { status: 500 });
+		}),
+	);
 
-  await expect(() =>
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).rejects.toThrow('Authentication failed: network error')
-})
+	await expect(() =>
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).rejects.toThrow('Authentication failed: network error');
+});
