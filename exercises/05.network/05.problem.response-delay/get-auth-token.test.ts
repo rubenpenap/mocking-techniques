9754b94d6751ca24e09dd5642ccc332f68016fd7
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-16 20:51:54 -0400

    test: add timeout handling test case for getAuthToken function

--- exercises/05.network/05.problem.response-delay/get-auth-token.test.ts
+++ exercises/05.network/05.problem.response-delay/get-auth-token.test.ts
@@ -1,70 +1,84 @@
-import { http } from 'msw'
-import { server } from './mocks/node.js'
-import { getAuthToken } from './get-auth-token.js'
+import { delay, http } from 'msw';
+import { server } from './mocks/node.js';
+import { getAuthToken } from './get-auth-token.js';
+
+beforeAll(() => {
+	vi.useFakeTimers();
+});
+
+afterAll(() => {
+	vi.useRealTimers();
+});
 
 test('returns the authentication token on successful authentication', async () => {
-  await expect(
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).resolves.toEqual({ token: 'abc-123' })
-})
+	await expect(
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).resolves.toEqual({ token: 'abc-123' });
+});
 
 test('throws an error if the user credentials are invalid', async () => {
-  server.use(
-    http.post('https://api.example.com/auth', () => {
-      return new Response(null, { status: 401 })
-    }),
-  )
+	server.use(
+		http.post('https://api.example.com/auth', () => {
+			return new Response(null, { status: 401 });
+		}),
+	);
 
-  await expect(() =>
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).rejects.toThrow('Authentication failed: invalid credentials')
-})
+	await expect(() =>
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).rejects.toThrow('Authentication failed: invalid credentials');
+});
 
 test('throws an error if the server responds with an error', async () => {
-  server.use(
-    http.post('https://api.example.com/auth', () => {
-      return new Response(null, { status: 500 })
-    }),
-  )
+	server.use(
+		http.post('https://api.example.com/auth', () => {
+			return new Response(null, { status: 500 });
+		}),
+	);
 
-  await expect(() =>
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).rejects.toThrow('Authentication failed: network error')
-})
+	await expect(() =>
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).rejects.toThrow('Authentication failed: network error');
+});
 
 test('handles network errors', async () => {
-  server.use(
-    http.post('https://api.example.com/auth', () => {
-      return Response.error()
-    }),
-  )
+	server.use(
+		http.post('https://api.example.com/auth', () => {
+			return Response.error();
+		}),
+	);
 
-  await expect(() =>
-    getAuthToken({
-      email: 'kody@epicweb.dev',
-      password: 'supersecret123',
-    }),
-  ).rejects.toThrow('Authentication failed: network error')
-})
+	await expect(() =>
+		getAuthToken({
+			email: 'kody@epicweb.dev',
+			password: 'supersecret123',
+		}),
+	).rejects.toThrow('Authentication failed: network error');
+});
 
-// 🐨 Create a new test case to check how the `getAuthToken` function
-// handler the request timeout.
-// 💰 test(title, callback)
+test('handles the request timeout', async () => {
+	server.use(
+		http.post('https://api.example.com/auth', async () => {
+			await delay('infinite');
+		}),
+	);
 
-// 🐨 In the new test case, add a runtime handler for the auth token
-// endpoint and respond to the request with the `delay` function.
-// Use `delay('infinite')` to make the request pend forever.
-// 💰 server.use(handler)
+	const tokenPromise = getAuthToken({
+		email: 'kody@epicweb.dev',
+		password: 'supersecret123',
+	});
 
-// 🐨 Next, complete the test by calling the `getAuthToken`
-// and writing the assertion around the correct rejection error.
-// 💰 You may want to mock *time* here.
+	vi.advanceTimersByTime(3000);
+
+	await expect(tokenPromise).rejects.toThrow(
+		'Authentication failed: request timed out',
+	);
+});
